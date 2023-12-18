9261c457a3c5406ce5d70026aa5ea65bd3a16c6e
VladPodilnyk <vladyslav.podilnyk@bolt.eu>
2023-12-18 08:28:46 +0200

    day 14

--- src/day14/day14-puzzle.py
+++ src/day14/day14-puzzle.py
@@ -1,82 +1,58 @@
-class Solver:
-    def __init__(self, input_data, m, n):
-        self.input_data = input_data
-        self.m = m
-        self.n = n
+from collections.abc import Callable
 
-    def debug(self):
-        cols = []
-        for j in range(len(self.input_data)):
-            col = []
-            spheres = self.input_data[j][0]
-            cubes = self.input_data[j][1]
-            for i in range(self.m):
-                if (i, j) in spheres:
-                    col.append('O')
-                elif (i, j) in cubes:
-                    col.append('#')
-                else:
-                    col.append('.')
-            cols.append(''.join(col))
-        [print(''.join(i)) for i in zip(*cols)]
+def transpose(grid: list[str]) -> list[str]:
+    return [''.join(i) for i in zip(*grid)]
 
-    def solve(self) -> int:
-        # self.debug()
+def apply_n_times(func: Callable[[str], str], data: str, n: int) -> str:
+    seen = {}
+    is_cycle = False
+    i = 0
+    while i < n:
+        if data in seen:
+            is_cycle = True
+            break
+        seen[data] = i
+        data = func(data)
+        i += 1
 
-        for col in range(len(self.input_data)):
-            self.move_rocks(col)
+    if not is_cycle:
+        return data
 
-        # self.debug()
-        res = 0
-        for col in range(len(self.input_data)):
-            res += self.calc_weight(col)
-        return res
+    cycle_start = seen[data]
+    cycle_length = i - cycle_start
+    left = (n - i) % cycle_length
+    return apply_n_times(func, data, left)
 
-    def calc_weight(self, col: int) -> int:
-        spheres = self.input_data[col][0]
-        weight = 0
-        for sphere in spheres:
-            weight += self.m - sphere[0]
-        return weight
+def move_line_east(line: str) -> str:
+    return '#'.join([''.join(sorted(part)) for part in line.split('#')])
 
-    def move_rocks(self, col: int):
-        spheres = self.input_data[col][0]
-        cubes = self.input_data[col][1]
+def move_east(grid: list[str]) -> list[str]:
+    return [move_line_east(line) for line in grid]
 
-        i = 0
-        j = 0
-        curr_row = 0
+def move_line_west(line: str) -> str:
+    return '#'.join([''.join(sorted(part, reverse=True)) for part in line.split('#')])
 
-        while i < len(spheres):
-            # move sphere to the north if there is no obstacle (cube)
-            if len(cubes) == 0 or j >= len(cubes):
-                spheres[i] = (curr_row, spheres[i][1])
-            elif spheres[i][0] < cubes[j][0]:
-                spheres[i] = (curr_row, spheres[i][1])
-            else:
-                while j < len(cubes) and spheres[i][0] > cubes[j][0]:
-                    j += 1
-                curr_row = cubes[j-1][0] + 1
-                spheres[i] = (curr_row, spheres[i][1])
-            curr_row += 1
-            i += 1
+def move_west(grid: list[str]) -> list[str]:
+    return [move_line_west(line) for line in grid]
 
+def move_north(grid: list[str]) -> list[str]:
+    return transpose(move_west(transpose(grid)))
+
+def move_south(grid: list[str]) -> list[str]:
+    return transpose(move_east(transpose(grid)))
+
+def exec_cycle(grid: list[str]) -> list[str]:
+    return move_east(move_south(move_west(move_north(grid))))
+
+def apply_transformation(data: str) -> str:
+    return '\n'.join(exec_cycle(data.splitlines()))
+
+def find_weigth(data: str, cycles: int) -> int:
+    transormed_data = apply_n_times(apply_transformation, data, cycles)
+    return sum(r * row.count('O') for r, row in enumerate(transormed_data.splitlines()[::-1], 1))
 
 if __name__ == '__main__':
     file = 'input.txt'
     with open(file, 'r') as reader:
-        grid = [line.strip() for line in reader.readlines()]
-
-        input_data = []
-        for j in range(len(grid)):
-            spheres = []
-            cubes = []
-            for i in range(len(grid[j])):
-                if grid[i][j] == 'O':
-                    spheres.append((i, j))
-                elif grid[i][j] == '#':
-                    cubes.append((i, j))
-            input_data.append([spheres, cubes])
-
-        solver = Solver(input_data, len(grid), len(grid[0]))
-        print('Result: {}'.format(solver.solve()))
+        data = reader.read()
+        print('Result: {}'.format(find_weigth(data, 1_000_000_000))) #1000000000
